bb02f4ecb566b71b7bdf43800646b3920d742693
Alberto Saltarelli <a.saltarelli93@icloud.com>
2017-02-25 19:09:56 +0100

    Working on LoginController

---
+++ IndusTrees/IndusTrees/LoginController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  IndusTrees
+//
+//  Created by Alberto Saltarelli on 25/02/2017.
+//  Copyright © 2017 Saltarelli. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    fileprivate static let logoMinY: CGFloat = UIApplication.shared.statusBarFrame.height * 2
+    
+    @IBOutlet weak var viewLogo: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.configure()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        self.animateView()
+    }
+    
+    @IBAction func loginDidTap(_ sender: UIButton) {
+        //instantiate MenuController according to privileges
+    }
+    
+    private func configure() {
+        self.viewLogo.center = self.view.center
+    }
+    
+    private func animateView() {
+    
+        UIView.animate(withDuration: 1.5, delay: 0, options: .curveEaseIn, animations: {
+            
+            let logoScaledWidth: CGFloat = self.viewLogo.frame.width / 2
+            
+            self.viewLogo.bounds.size = CGSize(width: logoScaledWidth, height: logoScaledWidth)
+            self.viewLogo.center.y = (logoScaledWidth / 2) + LoginController.logoMinY
+        }, completion: { _ in
+            
+            UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
+                
+                //AnimateTextField
+            }, completion: nil)
+        })
+    }
+    
+    
+}
---
+++ IndusTrees/IndusTrees/TeamController.swift
@@ -0,0 +1,49 @@
+//
+//  TeamController.swift
+//  IndusTrees
+//
+//  Created by Alberto Saltarelli on 25/02/2017.
+//  Copyright © 2017 Saltarelli. All rights reserved.
+//
+
+import UIKit
+
+class TeamController: UIViewController {
+    
+    @IBOutlet weak var memberTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.memberTableView.dataSource = self
+        self.memberTableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TeamController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+}
