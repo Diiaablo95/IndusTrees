385dc140943b4d8c3625f490846f2237b9e8b10d
Andrea Vultaggio <andre.vultaggio@gmail.com>
2017-02-26 05:20:36 +0100

    ProfileViewController for Employees is already done

--- IndusTrees/IndusTrees/ProfileViewController.swift
+++ IndusTrees/IndusTrees/ProfileViewController.swift
@@ -10,13 +10,36 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var Profile: UILabel!
+    @IBOutlet weak var foto: UIImageView!
+    @IBOutlet weak var nome: UILabel!
+    @IBOutlet weak var dataNascita: UILabel!
+    @IBOutlet weak var ruolo: UILabel!
+    @IBOutlet weak var level: UILabel!
+    @IBOutlet weak var xpPoints: UILabel!
+    @IBOutlet weak var task: UILabel!
+    
+    let account: Account? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let account = account else {
+            return
+        }
+        
+        nome.text = account.email
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    @IBAction func taskList(_ sender: UIButton) {
+    }
+    
+    @IBAction func achievmentsPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func teamSelected(_ sender: UIButton) {
+    }
 }
