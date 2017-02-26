d68d444be7a26008375672b428b08cff3873c7bf
Andrea Vultaggio <andre.vultaggio@gmail.com>
2017-02-26 04:20:59 +0100

    Committed AchievmentViewController and ProfileViewController

---
+++ IndusTrees/IndusTrees/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  IndusTrees
+//
+//  Created by Andrea Vultaggio on 26/02/2017.
+//  Copyright © 2017 Saltarelli. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
